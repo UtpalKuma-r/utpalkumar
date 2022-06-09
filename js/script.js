8230fb792f154ecdcd54150a8a0f5ef9bb613b67
EzyGrip <73326144+UtpalKuma-r@users.noreply.github.com>
2022-06-09 15:10:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("content").style.height = window.screen.height;
+
+let window_height = (window.screen.height).toString().concat("px");
+// alert(window_height, typeof(heightVar));
+document.getElementById("content").style.height = window_height;
